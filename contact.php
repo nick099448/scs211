8096a752ecc468166ef5e6b40c7e169eb7b30b29
nick099448 <dhffhvhch@gmail.com>
2023-03-05 17:57:42 +0700

    week13

---
+++ contact.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>หลักสูตรวิทยาการคอมพิวเตอร์</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta property="og:url" content="http://cs.vru.ac.th" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="หลักสูตรวิทยาการคอมพิวเตอร์ มหาวิทยาลัยราชภัฏวไลยอลงกรณ์ ในพระบรมราชูปถัมภ์ " />
+    <meta property="og:description" content="หลักสูตรวิทยาการคอมพิวเตอร์ คณะวิทยาศาสตร์และเทคโนโลยี มหาวิทยาลัยราชภัฏวไลยอลงกรณ์ ในพระบรมราชูปถัมภ์ " />
+    <meta property="og:image" content="http://cs.vru.ac.th/images/logo5.png" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,900|Oswald:300,400,700" rel="stylesheet">
+    <!--link href="https://fonts.googleapis.com/css2?family=Kanit:ital@1&display=swap" rel="stylesheet"-->
+    <link href="https://fonts.googleapis.com/css?family=Prompt" rel="stylesheet">
+    <!--link rel="stylesheet" href="css/bootstrap.min.css"-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <!--link rel="stylesheet" href="css/jquery-ui.css"-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css">
+    <!--link rel="stylesheet" href="css/owl.carousel.min.css"-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <!--link rel="stylesheet" href="css/owl.theme.default.min.css"-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+    <!--link rel="stylesheet" href="css/jquery.fancybox.min.css"-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css">
+    <!--link rel="stylesheet" href="css/bootstrap-datepicker.css"-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <!--link rel="stylesheet" href="css/aos.css"-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/brands.min.css"> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="https://cs.vru.ac.th/css/style.css">
+    <style>
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6,
+        nav,
+        .nav,
+        .menu,
+        button,
+        .button,
+        .btn,
+        .price,
+        ._heading,
+        .wp-block-pullquote blockquote,
+        blockquote,
+        label,
+        legend,
+        a,
+        .card-header,
+        th,
+        p,
+        li {
+            font-family: "Prompt", "Open Sans script=all rev=1" !important;
+            font-weight: 500 !important;
+
+        }
+
+        .site-footer {
+            padding: 4em !important;
+            background: #333333 !important;
+            /*height:300px !important;*/
+        }
+
+        .site-navbar {
+            margin-bottom: 0px;
+            z-index: 1999;
+            position: absolute;
+            width: 100%;
+            padding: 4px !important;
+            text-align: center !important;
+
+
+        }
+
+        .d-block {
+            display: block !important;
+            width: 50% !important;
+        }
+
+        .site-navbar .site-navigation .site-menu>li>a {
+            padding: 0 !important;
+            /*color: rgba(255, 255, 255, 0.5);*/
+            display: inline-block;
+            text-decoration: none !important;
+        }
+
+        .site-menu>.nav-link {
+            padding: 1.5rem 0.5rem;
+        }
+
+        .sticky-wrapper .site-navbar {
+            background: #fff;
+            border-bottom: 1px solid transparent;
+            -webkit-box-shadow: 4px 0 20px -5px rgba(0, 0, 0, 0.2);
+            box-shadow: 4px 0 20px -5px rgba(0, 0, 0, 0.2);
+        }
+
+        .site-navbar .site-navigation .site-menu>li>a {
+            color: black;
+
+        }
+
+        .site-navbar .site-navigation .site-menu>li>a:hover {
+            color: purple;
+        }
+
+        .sticky-wrapper .site-navbar ul li a {
+            color: black;
+        }
+
+        .sticky-wrapper .site-navbar ul li a:hover {
+            color: purple;
+        }
+
+        .content {
+            padding-top: 90px;
+            padding-bottom: 90px;
+        }
+
+        body {
+            line-height: 1.7;
+            color: #333;
+            font-weight: 400;
+            font-size: 1rem;
+
+        }
+
+        .anchor {
+            padding-top: 150px;
+            margin-top: -150px;
+        }
+    </style>
+    <link rel="icon" href="https://cs.vru.ac.th/images/css.png">
+    <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
+</head>
+
+<body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+    <div class="site-wrap">
+        <div class="site-mobile-menu site-navbar-target">
+            <div class="site-mobile-menu-header">
+                <div class="site-mobile-menu-close mt-3">
+                    <span class="icon-close2 js-menu-toggle"></span>
+                </div>
+                <img class="m-4" src="https://cs.vru.ac.th/images/css.png" height="70">
+            </div>
+            <div class="site-mobile-menu-body"></div>
+        </div>
+    </div>
+   
+    <!-- start of content , see end in footer -->
+    <div class="content">
+        <div class="container">
+            <br />
+            <h2>&starf;&nbsp;ติดต่อเรา</h2>
+            <hr>
+
+            <style>
+                #map_container {
+                    height: 250px;
+                    margin-bottom: 10px;
+                }
+
+                #map {
+                    width: 100%;
+                    height: 100%;
+                }
+            </style>
+            <div id="map_container">
+                <div id="map"></div>
+            </div>
+            <script>
+                var map;
+
+                function initMap() {
+                    map = new google.maps.Map(document.getElementById('map'), {
+                        center: {
+                            lat: 14.132371,
+                            lng: 100.616594
+                        },
+                        zoom: 15
+                    });
+                    var marker = new google.maps.Marker({
+                        position: new google.maps.LatLng(14.132371, 100.616594),
+                        map: map,
+                        title: 'หลักสูตรวิทยาการคอมพิวเตอร์ มหาวิทยาลัยราชภัฏวไลยอลงกรณ์ฯ'
+                    });
+                }
+            </script>
+            <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCoZlRLPlMJTXe38xtlDFiyI9rj6BoG10M&callback=initMap" async defer></script>
+
+            <p></p>
+            <h4>หลักสูตรวิทยาการคอมพิวเตอร์</h4>
+            <p>&nbsp; &nbsp; &nbsp; &nbsp;อาคาร 75 ปี วไลยอลงกรณ์ </p>
+            <p>&nbsp; &nbsp; &nbsp; &nbsp;มหาวิทยาลัยราชภัฏวไลยอลงกรณ์ ในพระบรมราชูปถัมภ์</p>
+            <p>&nbsp; &nbsp; &nbsp; &nbsp;เลขที่ 1 หมู่ 20 ถนนพหลโยธิน กม. 48 ต.คลองหนึ่ง อ.คลองหลวง จ.ปทุมธานี 13180</p>
+            <p>&nbsp; &nbsp; &nbsp; &nbsp;โทรศัพท์ 0-2529-0674-7 ต่อ 365 โทรสาร 02-909-3029</p>
+            &nbsp; &nbsp; &nbsp; &nbsp;<a href="https://www.facebook.com/profile.php?id=100008113098809&fref=ts"><img src="https://cs.vru.ac.th/images/facebook-icon-preview-1.png" width="50" height="50"> FACEBOOK หลักสูตรวิทยาการคอมพิวเตอร์</a>
+        </div>
+    </div>
+    <!-- end of content, see start in header -->
+   
+    <div class="container-fluid bg-light  p-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 pl-4">
+                    <h3>สมัครเรียนกับเรา</h3>
+                    <h5 id="schedule">สอบถามข้อมูลการรับสมัครเพิ่มเติม</h5>
+                    <p>โทร. 02-909-1432 ต่อ 322 หรือ 323</p>
+                </div>
+                <div class="col-md-6 text-right">
+                    <div class="py-4">
+                        <a class="btn btn-primary" href="tcas.php">รายะละเอียดสมัครเรียน</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <!-- Cookie Consent by https://www.cookiewow.com -->
+
+    <!-- Load Facebook SDK for JavaScript -->
+    <div id="fb-root"></div>
+   
+
+    <!-- Your Chat Plugin code -->
+    <div class="fb-customerchat" attribution=setup_tool page_id="2105621063039483" logged_in_greeting="สวัสดี ... เราสามารถช่วยเหลืออะไรคุณได้บ้าง บอกเราได้นะ?" logged_out_greeting="สวัสดี ... เราสามารถช่วยเหลืออะไรคุณได้บ้าง บอกเราได้นะ?">
+    </div>
+
+    
+</body>
+
+</html>
